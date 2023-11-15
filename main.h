e9172e11509520add144d1287819cac3ec8ad928
teresiawairimu <nduatiteresia82@gmail.com>
2023-11-15 10:56:01 -0800

    Updated main.h with the function to print char

--- main.h
+++ main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
+int printf_char(va_list val);
 int _putchar(char c);
 int _print_string(char *str);
 int _print_int(int num);
