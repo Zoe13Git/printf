e851e342ba7da5ec073d6a546ffffa2ffec2e3c7
teresiawairimu <nduatiteresia82@gmail.com>
2023-11-15 11:30:54 -0800

    main.h

--- main.h
+++ main.h
@@ -2,9 +2,13 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stddef.h>
 
 int _printf(const char *format, ...);
 int printf_char(va_list val);
+int printf_string(va_list val);
+int _strlen(char *str);
+int _strlenc(const char *str);
 int _putchar(char c);
 int _print_string(char *str);
 int _print_int(int num);
